5f39c16e93f58111fb79eca600d4084069840544
Mac Pro <macpro@Macs-Mac-Pro.local>
2026-02-03 21:32:43 +0200

    Fixed image display issues by updating components to use available placeholder images

--- components/welcome-avatar.tsx
+++ components/welcome-avatar.tsx
@@ -47,7 +47,7 @@ export function WelcomeAvatar() {
           {/* الأيقونة */}
           <div className="flex justify-center">
             <img
-              src="/images/d8-aa-d8-b5-d9-85-d9-8a-d9-85-20-d8-a8-d8-af-d9-88-d9-86-20-d8-b9-d9-86-d9-88-d8-a7-d9-86-20-281-29.jpeg"
+              src="/placeholder.jpg"
               alt="شوقي جروب - فريق العمل"
               className="w-48 h-auto rounded-2xl shadow-2xl shadow-primary/40 object-cover"
             />
